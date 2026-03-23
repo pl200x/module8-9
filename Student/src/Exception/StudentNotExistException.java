5b9ce035dcd5607d4ea6043036045e6b7601353f
pl200x <pw2663@columbia.edu>
2026-03-23 19:40:57 -0400

    Add files via upload

---
+++ Student/src/Exception/StudentNotExistException.java
@@ -0,0 +1,5 @@
+package Exception;
+
+public class StudentNotExistException extends RuntimeException{
+    public StudentNotExistException(String s){super (s);}
+}
---
+++ Student/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Student test = new Student();
+       Scanner sc = new Scanner(System.in);
+       
+
+       sc.close();
+    }
+}
